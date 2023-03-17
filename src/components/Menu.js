5f31532420944d723548bd2a40cd50046b5fda33
Kickstartlab <info@kickstartlabs.io>
2023-03-18 01:00:09 +0500

    update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -80,6 +80,12 @@ export default function Menu() {
 
       <div className="lg:hidden fixed bg-gray-100 w-full p-2 z-20 flex items-center justify-between gap-x-3 text-white-100 left-0 top-0">
 
+      <button onClick={() => setShow(!show)} className="nav cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-white-100">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
+          </svg>
+        </button>
+
         {account ? (
           <button
             onClick={() => disconnect()}
@@ -96,11 +102,6 @@ export default function Menu() {
           </button>
         )}
 
-        <button onClick={() => setShow(!show)} className="nav cursor-pointer">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-white-100">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
-          </svg>
-        </button>
       </div>
 
       {
